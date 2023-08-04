ab8046fc93d13f0be65019b83d1b62963c0dc366
abdifatah <abdifatahshukri15@gmail.com>
2023-08-05 00:42:52 +0300

    updated

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// function fetchBooks(){
   
-}
+//   fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then((json) => console.log(json));
+// }
+// function renderBooks(book) {
+//   const main = document.querySelector('main');
+//   json.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = `<h2>${book.name}</h2>`;
+//     main.appendChild(h2);
+//   });
+// }
 
+// document.addEventListener("DOMContentLoaded", function() {
+//   fetchBooks();
+// });
+
+
+function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+  // To pass the tests, don't forget to return your fetch!
+
+}
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,7 +33,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
